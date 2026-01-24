ce35c7f1d4f1541f032555d25f8ee8b851185445
issnehaaa <issnehaaa@gmail.com>
2026-01-24 18:34:00 +0530

    Add files via upload

---
+++ numtriangle.cpp
@@ -0,0 +1,22 @@
+//number triangle
+
+#include <iostream>
+using namespace std;
+void NumberTriangle(int n) {
+        
+        for (int i=1;i<=n;i++ ){
+        for (int j=1;j<=i;j++){
+          
+            cout<<j;
+        
+        }
+        cout<< " "<< endl;
+    }
+
+}
+int main (){
+    int n;
+    NumberTriangle(6);
+
+return 0;
+}
